24e3e9f6a68929484f20c6c50f5b452a6ffe9c0d
Vikas Badoni <38487818+vpbadoni@users.noreply.github.com>
2019-11-30 10:30:44 +0530

    init commitfor connect

---
+++ GoogleDriveWebApp/Default.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GoogleDriveWebApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .ContentDiv {
+            width: 100%;
+            margin: auto 0;
+        }
+
+        .innerdiv {
+            width: 80%;
+            margin: auto 0;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <div class="ContentDiv">
+                    <div class="innerdiv">
+                        <asp:GridView ID="gridService" runat="server" CssClass="EU_DataTable" AutoGenerateColumns="false" ShowFooter="true" OnRowCommand="gridService_RowCommand">
+                            <Columns>
+                                <asp:TemplateField ItemStyle-Width="30px" HeaderText="SR.NO">
+                                    <ItemTemplate>
+                                        <asp:Label ID="lblID" runat="server"
+                                            Text='<%#Eval("Id")%>'></asp:Label>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField ItemStyle-Width="600px" HeaderText="Service">
+                                    <ItemTemplate>
+                                        <asp:Label ID="lblService222" runat="server" Text='<%#Eval("Name")%>'></asp:Label>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField ItemStyle-Width="600px" HeaderText="Service">
+                                    <ItemTemplate>
+                                        <asp:Label ID="lblService22" runat="server" Text='<%#Eval("Size")%>'></asp:Label>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField ItemStyle-Width="600px" HeaderText="Service">
+                                    <ItemTemplate>
+                                        <asp:Label ID="lblService2" runat="server" Text='<%#Eval("CreatedTime")%>'></asp:Label>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField>
+                                    <ItemTemplate>
+                                        <asp:LinkButton ID="lnkRemove" CommandName="DownloadFile" runat="server" CommandArgument='<%#  
+                                         Eval("Id")%>'
+                                            OnClientClick="return confirm('Do you want to delete?')"
+                                            Text="Download"></asp:LinkButton>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+
+                            </Columns>
+                        </asp:GridView>
+                    </div>
+                </div>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ GoogleDriveWebApp/Default.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+using System.Data;
+using System.Reflection;
+
+namespace GoogleDriveWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public static string[] Scopes = { DriveService.Scope.Drive };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                List<GoogleDriveFiles> FileList = GetDriveFiles();
+                DataTable dtlist = ToDataTable(FileList);
+                if (dtlist.Rows.Count > 0)
+                {
+                    gridService.DataSource = dtlist;
+                    gridService.DataBind();
+                }
+            }
+           
+        }
+
+        public DataTable ToDataTable<T>(List<T> items)
+
+        {
+
+            DataTable dataTable = new DataTable(typeof(T).Name);
+
+            //Get all the properties
+
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo prop in Props)
+
+            {
+
+                //Setting column names as Property names
+
+                dataTable.Columns.Add(prop.Name);
+
+            }
+
+            foreach (T item in items)
+
+            {
+
+                var values = new object[Props.Length];
+
+                for (int i = 0; i < Props.Length; i++)
+
+                {
+
+                    //inserting property values to datatable rows
+
+                    values[i] = Props[i].GetValue(item, null);
+
+                }
+
+                dataTable.Rows.Add(values);
+
+            }
+
+            //put a breakpoint here and check datatable
+
+            return dataTable;
+
+        }
+        public static DriveService GetService()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream(@"D:\credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                String FolderPath = @"D:\";
+                String FilePath = Path.Combine(FolderPath, "DriveServiceCredentials.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(FilePath, true)).Result;
+            }
+
+            //Create Drive API service.
+            DriveService service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GoogleDriveRestAPI-v3",
+            });
+
+            return service;
+        }
+
+        public static List<GoogleDriveFiles> GetDriveFiles()
+        {
+            DriveService service = GetService();
+
+            // Define parameters of request.
+            FilesResource.ListRequest FileListRequest = service.Files.List();
+
+            //listRequest.PageSize = 10;
+            //listRequest.PageToken = 10;
+            FileListRequest.Fields = "nextPageToken, files(id, name, size, version, trashed, createdTime)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = FileListRequest.Execute().Files;
+            List<GoogleDriveFiles> FileList = new List<GoogleDriveFiles>();
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    GoogleDriveFiles File = new GoogleDriveFiles
+                    {
+                        Id = file.Id,
+                        Name = file.Name,
+                        Size = file.Size,
+                        Version = file.Version,
+                        CreatedTime = file.CreatedTime
+                    };
+                    FileList.Add(File);
+                }
+            }
+            return FileList;
+        }
+
+        public static string DownloadGoogleFile(string fileId)
+        {
+            DriveService service = GetService();
+
+            string FolderPath = System.Web.HttpContext.Current.Server.MapPath("/GoogleDriveFiles/");
+            FilesResource.GetRequest request = service.Files.Get(fileId);
+
+            string FileName = request.Execute().Name;
+            string FilePath = System.IO.Path.Combine(FolderPath, FileName);
+
+            MemoryStream stream1 = new MemoryStream();
+
+            request.MediaDownloader.ProgressChanged += (Google.Apis.Download.IDownloadProgress progress) =>
+            {
+                switch (progress.Status)
+                {
+                    case DownloadStatus.Downloading:
+                        {
+                            //Console.WriteLine(progress.BytesDownloaded);
+                            break;
+                        }
+                    case DownloadStatus.Completed:
+                        {
+                            //Console.WriteLine("Download complete.");
+                            SaveStream(stream1, FilePath);
+                            break;
+                        }
+                    case DownloadStatus.Failed:
+                        {
+                            //Console.WriteLine("Download failed.");
+                            break;
+                        }
+                }
+            };
+            request.Download(stream1);
+            return FilePath;
+        }
+
+        private static void SaveStream(MemoryStream stream, string FilePath)
+        {
+            using (System.IO.FileStream file = new FileStream(FilePath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                stream.WriteTo(file);
+            }
+        }
+
+        protected void gridService_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName== "DownloadFile")
+            {
+                string id = e.CommandArgument.ToString();
+                string FilePath = DownloadGoogleFile(id);
+
+
+                Response.ContentType = "application/zip";
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+                Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+                Response.End();
+                Response.Flush();
+            }
+        }
+    }
+}
